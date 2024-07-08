2ebfcd211110ec5e52e8e5d82f1125ca33e146d7
vigashni21 <cssrivigashni@gmail.com>
2024-07-08 11:25:14 +0530

    Add files via upload

---
+++ swap_string_var.js
@@ -0,0 +1,10 @@
+let a = "leena";
+let b = "roshini";
+ 
+console.log(`before swap a= ${a}`);
+console.log(`before swap a= ${b}`);
+ 
+[b, a] = [a, b];
+ 
+console.log(`after swap a= ${a}`);
+console.log(`after swap a= ${b}`);
